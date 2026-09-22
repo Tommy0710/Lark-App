8a8a2f8d4db3a5bdbe1d252e78318c0350916336
Nguyen Van Thien <thiennguyen0710.dn@gmail.com>
2026-09-22 08:53:54 +0700

    update translation 22/9/2026

--- translations.js
+++ translations.js
@@ -1,39 +1,20 @@
+// Key = mô tả đã bỏ tiền tố "A pair of" / "A piece of" (xem cleanProductKey)
 const exactTranslationMap = {
-  'Sully goatskin leather watch straps Violine': 'Da dê vân sully màu Violine',
-  'Epsom calfskin leather watch straps Rouge': 'Da bê vân Epsom HAAS Màu Đỏ (Rouge)',
-  'Sully goatskin leather watch straps Rose': 'Da dê vân sully màu Rose',
-  'Epsom calfskin leather watch straps Navy Blue': 'Da bê vân Epsom HAAS Màu Xanh Navy (Navy Blue)',
-  'Sully goatskin leather watch straps Light Cream': 'Da dê vân Sully Màu Light Cream',
-  'Epsom calfskin leather watch straps Gold': 'Da bê vân Epsom Màu Nâu vàng (Gold)',
-  'Lagun cowhide leather watch straps Ficelle': 'Da bò vân Lagun Màu Trắng Đục (Ficelle)',
-  'Sully goatskin leather watch straps Noisette': 'Da dê vân Sully Màu Noisette',
-  'Sully grain goatskin leather wallet Violine': 'Da dê vân sully màu Violine',
-  'Sully goatskin leather wallet Violine': 'Da dê vân sully màu Violine',
-  'Epsom calfskin leather wallet Rouge': 'Da bê vân Epsom HAAS Màu Đỏ (Rouge)',
-  'Sully grain goatskin leather wallet Rose': 'Da dê vân sully màu Rose',
-  'Sully goatskin leather wallet Rose': 'Da dê vân sully màu Rose',
-  'Epsom calfskin leather wallet Navy Blue': 'Da bê vân Epsom HAAS Màu Xanh Navy (Navy Blue)',
-  'Sully goatskin leather wallet Light Cream': 'Da dê vân Sully Màu Light Cream',
-  'Epsom calfskin leather wallet Gold': 'Da bê vân Epsom Màu Nâu vàng (Gold)',
-  'Lagun cowhide leather wallet Ficelle': 'Da bò vân Lagun Màu Trắng Đục (Ficelle)',
-  'Sully goatskin leather wallet Noisette': 'Da dê vân Sully Màu Noisette',
-  'Epsom calfskin leather watch straps HAAS Cream': 'Da bê vân Epsom HAAS Màu Kem (Cream)',
-  'Epsom calfskin leather watch straps Chocolate': 'Da bê vân Epsom Màu Chocolate',
-  'Sully goatskin leather watch straps Acier': 'Da dê vân Sully Màu Acier',
-  'Patnat goatskin leather watch straps Patchouli': 'Da dê vân Patnat màu Patechouli',
-  'Lagun cowhide leather watch straps Orange': 'Da bò vân Lagun Màu Cam (Orange)',
-  'Epsom calfskin leather watch straps Noir black': 'Da bê vân Epsom Màu Đen (Noir)',
-  'Togo Odessa Weinheimer calfskin leather watch straps black': 'Da bê Odessa vân Togo weinheimer màu đen black',
-  'Sully goatskin leather watch straps Bleu Vista': 'Da dê vân Sully Màu Bleu Vista',
-  'Epsom calfskin leather wallet Etoupe': 'Da bê vân Epsom Màu Ghi Nâu (Etoupe)',
-  'Sully goatskin leather wallet Myrrhe': 'Da dê vân Sully Màu Myrrhe',
-  'Sully grain goatskin leather wallet Myrrhe': 'Da dê vân Sully Màu Myrrhe',
-  'Sully grain goatskin leather wallet Colibri': 'Da dê vân Sully Màu Colibri',
-  'Sully goatskin leather wallet Colibri': 'Da dê vân Sully Màu Colibri',
-  'Epsom calfskin leather wallet HAAS Cream': 'Da bê vân Epsom HAAS Màu Kem (Cream)',
-  'Epsom calfskin leather wallet Chocolate': 'Da bê vân Epsom Màu Chocolate',
-  'Sully goatskin leather wallet Oursin': 'Da dê vân Sully Màu Oursin',
-  'Sully grain goatskin leather wallet Oursin': 'Da dê vân Sully Màu Oursin'
+  // Dây đeo đồng hồ (Pair)
+  'epsom calfskin watch strap weinheimer black': 'Da bê vân epsom weinheimer màu đen',
+  'zermatt calfskin watch strap': 'Da bê zermat',
+  'sully goatskin watch strap orange': 'Da dê vân sully màu cam (orange)',
+  'epsom calfskin leather watch straps navy blue': 'Da bê vân epsom haas màu xanh navy (navy blue)',
+  'cowhide leather watch strap Jewel': 'Da bò thuộc mã MA230007 Jewel',
+  'epsom calfskin leather watch straps rouge': 'Da bê vân epsom haas màu đỏ (rouge)',
+
+  // Ví (Piece)
+  'swift calfskin wallet': 'Da bê swift',
+  'Sully goatskin wallet noir black': 'Da dê vân sully màu noir',
+  'Sully goatskin wallet light cream': 'Da dê vân sully màu light cream',
+  'cowhide wallet agave': 'Da bò thuộc mã MA660021 agave',
+  'zermatt calfskin wallet': 'Da bê zermat',
+  'Epsom calfskin wallet haas cream': 'Da bê vân epsom haas màu kem (cream)'
 };
 
 function normalizeText(text) {
